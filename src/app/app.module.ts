ba3c152f4c9e59be0d92cf6ef094273e1c75218c
AcrixF <acrixf@gmail.com>
2017-04-27 16:58:06 -0500

    Finalizado!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http, RequestOptions } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+
+import { provideAuth, AuthHttp, AuthConfig } from 'angular2-jwt'
+
+//Rutas
+import { APP_ROUTING } from './app.routes';
+//Servicios
+import { Auth } from './services/auth.service';
+import {AuthGuardService} from "./services/auth-guard.service";
+
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions){
+  return new AuthHttp(new AuthConfig({}), http, options);
+}
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PreciosComponent,
+    ProtegidaComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    Auth,
+    AuthGuardService,
+    {
+      provide:AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [Http, RequestOptions]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
